3f23f5a07c264731daced4ce99080b1473b91084
tinysakura <2796191719@qq.com>
2018-10-06 17:15:17 +0800

    测试thymeleaf表单的提交

--- src/main/java/com/cfh/study/studythymeleaf/controller/UserController.java
+++ src/main/java/com/cfh/study/studythymeleaf/controller/UserController.java
@@ -3,10 +3,13 @@ package com.cfh.study.studythymeleaf.controller;
 import com.cfh.study.studythymeleaf.pojo.User;
 import com.cfh.study.studythymeleaf.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -46,5 +49,18 @@ public class UserController {
         model.addAttribute("user", user);
 
         return "showOneUser";
+    }
+
+    @RequestMapping("/add/form")
+    public String addForm() {
+        return "form";
+    }
+
+    @PostMapping("/add")
+    @ResponseBody
+    public String addUser(User user) {
+        log.info("user:{}", user.toString());
+
+        return "add success";
     }
 }
